5ca413a2bd30dcdcd2883f2b18bae3072e32154d
TJRobson <thomas.j.robson@hotmail.com>
2017-08-04 21:26:25 +1200

    Back from Thailand. Working solution for func runSimulation.

--- problem_set_2/ps2.py
+++ problem_set_2/ps2.py
@@ -15,7 +15,7 @@ import pylab
 # If you get a "Bad magic number" ImportError, you are not using Python 3.5 
 
 # For Python 3.6:
-from ps2_verify_movement36 import testRobotMovement
+#from ps2_verify_movement36 import testRobotMovement
 # If you get a "Bad magic number" ImportError, you are not using Python 3.6
 
 
@@ -241,7 +241,7 @@ class StandardRobot(Robot):
             
 
 # Uncomment this line to see your implementation of StandardRobot in action!
-testRobotMovement(StandardRobot, RectangularRoom)
+#testRobotMovement(StandardRobot, RectangularRoom)
 
 
 # === Problem 4
@@ -263,10 +263,21 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     robot_type: class of robot to be instantiated (e.g. StandardRobot or
                 RandomWalkRobot)
     """
-    raise NotImplementedError
+    results = list()
+    for i in range(num_trials):
+        steps = 0
+        room = RectangularRoom(width, height)
+        robots = [robot_type(room, speed) for j in range(num_robots)]
+        while (room.getNumCleanedTiles()/room.getNumTiles()) < min_coverage:
+            steps += 1
+            for bot in robots:
+                bot.updatePositionAndClean()
+            if (room.getNumCleanedTiles()/room.getNumTiles()) >= min_coverage:
+                results.append(steps)
+    return sum(results)/len(results)
 
 # Uncomment this line to see how much your simulation takes on average
-##print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
+print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
 
 
 # === Problem 5
