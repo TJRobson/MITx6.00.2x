2d81430c4a8fb9ca0c8304d035302d34ceda0687
TJRobson <thomas.j.robson@hotmail.com>
2017-08-09 23:02:07 +1200

    No big changes. Added a function that gives the CoefficientOfVariation.

--- problem_set_3/exercises.py
+++ problem_set_3/exercises.py
@@ -10,10 +10,10 @@ def stdDevOfLengths(L):
     returns: float, the standard deviation of the lengths of the strings,
       or NaN if L is empty.
     """
-    lens, length = [len(l) for l in L], float(len(L))
+    length = float(len(L))
     if length == 0:
         return float('NaN')
-    
+    lens = [len(l) for l in L]
     mean = sum(lens)/length
     quant = [(m - mean)**2 for m in lens]
     total = sum(quant)
@@ -30,3 +30,14 @@ def stdDevOfLengths(L):
 L = ['apples', 'oranges', 'kiwis', 'pineapples'] 
 print(stdDevOfLengths(L))
 #should return 1.8708.
+
+def getCoefficientOfVariation(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot/len(X))**0.5
+    return std/mean
+
+N = [10, 4, 12, 15, 20, 5] 
+print(getCoefficientOfVariation(N))
