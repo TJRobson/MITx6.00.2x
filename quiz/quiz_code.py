b45f2eb84a63321476ba02056255b3cf119f4e03
TJRobson <thomas.j.robson@hotmail.com>
2017-08-23 22:55:37 +1200

    Finished song_playlist func for quiz. Probably should have used a for loop.

--- quiz/quiz_code.py
+++ quiz/quiz_code.py
@@ -10,3 +10,29 @@ def song_playlist(songs, max_size):
     Returns: a list of a subset of songs fitting in 'max_size' in the order 
              in which they were chosen.
     """
+    play_list, space_left = list(), 0   
+    songs_cpy = songs[:1] + sorted(songs[1:], key=lambda x: x[2])
+    
+    i = 0  
+    while not i > (len(songs)-1) and space_left < max_size:
+        
+        temp_size = space_left + songs_cpy[i][2]    
+        
+        if temp_size <= max_size:
+            play_list.append(songs_cpy[i][0])
+            space_left = temp_size
+            i += 1     
+        else:            
+            break  
+        
+    return play_list
+
+songs = [('Roar',4.4, 4.0),('Sail',3.5, 7.7),('Timber', 5.1, 6.9),('Wannabe',2.7, 1.2)]  
+max_size = 12.2
+# Returns ['Roar','Wannabe','Timber']
+
+print(song_playlist(songs, max_size))
+print(song_playlist([('a', 4.4, 4.0), ('b', 3.5, 7.7), ('c', 5.1, 6.9), ('d', 2.7, 1.2)], 20))
+print(song_playlist([('a', 4.4, 4.0), ('b', 3.5, 7.7), ('c', 5.1, 6.9), ('d', 2.7, 1.2)], 11))
+print(song_playlist([('aa', 4, 4), ('bb', 5, 7), ('cc', 5, 6), ('dd', 2, 1)], 3))
+print(song_playlist([('z', 0.1, 9.0), ('a', 4.4, 5.0), ('b', 2.7, 7.2), ('cc', 3.5, 7.7), ('ddd', 5.1, 6.9)], 14))
